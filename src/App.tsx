c15e5aa9cb73a38d84dd669cfb049dcf1fb61da9
Aderval <adervalteixeira@gmail.com>
2023-01-24 01:11:19 -0300

    criado a estrutura em react

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { Box, createTheme } from '@mui/material';
+import { ThemeProvider } from '@mui/system';
+import * as React from 'react';
+import { Header } from './components/Header';
+import { Layout } from './components/Layout';
+
+const theme = createTheme({});
+
+function App(){
+ return (
+ <ThemeProvider theme={theme}>
+<Box
+component="main"
+sx={{
+  height: "100vh",
+  background: "white",
+}}
+
+>
+  <Header/>
+  <Layout>
+    <h1>Olá Mundo! </h1>
+  </Layout>
+</Box>
+ </ThemeProvider>
+  
+ );
+
+}
+
+export default App;
