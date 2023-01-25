b4d52e01767ebcf058e8b2931b2c5cc6e2eef708
Aderval <adervalteixeira@gmail.com>
2023-01-25 16:29:11 -0300

    Criado tema e adicionado rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,49 @@
-import { ThemeProvider } from '@mui/system';
-import * as React from 'react';
+import { Typography } from "@mui/material";
+import { Box, ThemeProvider } from '@mui/system';
 import { Header } from './components/Header';
 import { Layout } from './components/Layout';
+import { appTheme } from './config/theme';
+import { Routes, Route, Link } from "react-router-dom";
 
-const theme = createTheme({});
+const Home = () => (
+  <Box>
+    <Typography variant="h3" component="h1">
+      Home
+    </Typography>
+  </Box>
+);
 
-function App(){
- return (
- <ThemeProvider theme={theme}>
-<Box
-component="main"
-sx={{
-  height: "100vh",
-  background: "white",
-}}
+const About = () => (
+  <Box>
+    <Typography variant="h3" component="h1">
+      About
+    </Typography>
+  </Box>
+);
 
->
-  <Header/>
-  <Layout>
-    <h1>Olá Mundo! </h1>
-  </Layout>
-</Box>
- </ThemeProvider>
-  
- );
+function App() {
+  return (
+    <ThemeProvider theme={appTheme}>
+      <Box
+        component="main"
+        sx={{
+          height: "100vh",
+          backgroundColor: (theme) => theme.palette.grey[900],
+        }}
+
+      >
+        <Header />
+        <Layout>
+          <h1>Welcome to react Router!</h1>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Routes>
+        </Layout>
+      </Box>
+    </ThemeProvider>
+
+  );
 
 }
 
