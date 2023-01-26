b92f270f5eedf0561b2dac23d3f36e02d20a7cbf
Aderval <adervalteixeira@gmail.com>
2023-01-26 16:19:32 -0300

    criado pagina do CRUD de categoria

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,10 @@ import { Box, ThemeProvider } from '@mui/system';
 import { Header } from './components/Header';
 import { Layout } from './components/Layout';
 import { appTheme } from './config/theme';
-import { Routes, Route, Link } from "react-router-dom";
-
-const Home = () => (
-  <Box>
-    <Typography variant="h3" component="h1">
-      Home
-    </Typography>
-  </Box>
-);
-
-const About = () => (
-  <Box>
-    <Typography variant="h3" component="h1">
-      About
-    </Typography>
-  </Box>
-);
+import { Routes, Route } from "react-router-dom";
+import { CategoryList } from "./features/categories/ListCategory";
+import { CategoryCreate } from "./features/categories/CreateCategory";
+import { CategoryEdit } from './features/categories/EditCategory';
 
 function App() {
   return (
@@ -30,14 +17,22 @@ function App() {
           height: "100vh",
           backgroundColor: (theme) => theme.palette.grey[900],
         }}
-
       >
         <Header />
         <Layout>
-          <h1>Welcome to react Router!</h1>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="about" element={<About />} />
+            <Route path="/" element={<CategoryList />} />
+            <Route path="/categories" element={<CategoryList />} />
+            <Route path="/categories/create" element={<CategoryCreate />} />
+            <Route path="/categories/edit/:id" element={<CategoryEdit />} />
+
+            <Route path="*" element={
+              <Box sx={{color: "white"}} >
+                <Typography variant="h1">404</Typography>
+                <Typography variant="h2">Page not found</Typography>
+              </Box>
+            } 
+            />
           </Routes>
         </Layout>
       </Box>
